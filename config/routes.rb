e95354fc73d071ab13bb5123198c6454eb66b055
CRESCO <murielberthoucrestey@gmail.com>
2017-02-08 00:46:14 +0100

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/trees'
+
+  get 'pages/pairs'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
